bd07d27faa38342e1da60b7fd2355cdba7e7ad44
Vijay Patil <patilvijayg@yahoo.com>
2015-09-19 11:43:39 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact from Message.')
+    end
+end
